6aa4c55465fde3d5ae07ebd450582feef05982d3
unknown <E.S.TECH>
2018-12-30 19:45:56 -0200

    test

---
+++ src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css';
+import 'modules/font-awesome/css/font-awesome.min.css';
+
+import React from 'react';
+import HealthStatus from '../template/healthStatus';
+
+export default props => (
+    <div className='container'>
+        <HealthStatus />
+    </div>
+)
